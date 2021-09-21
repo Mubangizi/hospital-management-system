0bab04d9c81d81c574d42c097d3ce74662d7d5be
mubangizi <mubangizia22@gmail.com>
2021-09-21 23:05:00 +0300

    add dashbard layout

---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { Avatar, IconButton } from "@material-ui/core";
+import MenuOutlinedIcon from "@material-ui/icons/MenuOutlined";
+import "./SideBar.css";
+import CloseOutlinedIcon from "@material-ui/icons/CloseOutlined";
+
+const SideBar = () => {
+  const [open, setOpen] = React.useState(false);
+  const history = useHistory();
+
+  const handleDrawer = () => {
+    if (open === true) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
+
+  const sendToHome = () => {
+    history.push("/");
+  };
+  return (
+    <div className="SideBar">
+      <div className={`SideBarContent Side-${open}`}>
+        <div>
+          <div className="UserImage">
+            <IconButton className="UserImageBtn" onClick={sendToHome}>
+              {/* <Avatar src={picture} /> */}
+              <div className="UserImageOverlay">
+                <p className="UserImageText">Mubangizi Allan</p>
+              </div>
+            </IconButton>
+            <div className="UserGitProfile ItemUnderline">Administrator</div>
+          </div>
+          <div className="NavItems">
+            <NavLink to={{ pathname: `/` }} exact={true}>
+              Dashboard
+            </NavLink>
+            <NavLink to={{ pathname: `/patients` }}>Patients</NavLink>
+            <NavLink to={{ pathname: `/departments` }}>Departments</NavLink>
+            <NavLink to={{ pathname: `/doctors` }}>Doctors</NavLink>
+            <NavLink to={{ pathname: `/nurses` }}>Nurses</NavLink>
+            <NavLink to={{ pathname: `/record_managers` }}>
+              Record Managers
+            </NavLink>
+            <NavLink to={{ pathname: `/settings` }}>Settings</NavLink>
+          </div>
+        </div>
+        <div className="RemarksSection">
+          <p>© 2021 HMS</p>
+        </div>
+      </div>
+      <div className="SideControllerBtn">
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawer}
+          edge="start"
+        >
+          {!open ? <MenuOutlinedIcon /> : <CloseOutlinedIcon />}
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <div className="Container">
+        <div className="HomeMainContent">
+          <h1>hey</h1>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default HomePage;
